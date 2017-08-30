c6b03caa9422dd7985c30215bf7805ef053393ee
jdguzmans <jdguzmans@hotmail.com>
2017-08-30 01:20:06 -0500

    done

--- js/main.js
+++ js/main.js
@@ -8,12 +8,25 @@ function main () {
 
     if (!$this.hasClass('active')) {
       $this.addClass('active')
+      console.log(this)
     }
   })
 
   $('.navbar-brand').click(function () {
     $('.nav li').removeClass('active')
     $('#homeNav').addClass('active')
+  })
+
+  $(window).on('scroll', function () {
+    $('.container').each(function () {
+      if ($(window).scrollTop() >= $(this).offset().top) {
+        $('.nav li').removeClass('active')
+        var id = $(this).attr('id')
+        console.log(id)
+        if (id === undefined) console.log(this)
+        $('#' + id + 'Nav').addClass('active')
+      }
+    })
   })
 }
 
